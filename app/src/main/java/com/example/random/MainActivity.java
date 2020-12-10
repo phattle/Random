4f43cf7b98d50a524eeed9ba1e2a4e758ab08b63
Phát Lê <lethanhphat1408@gmail.com>
2020-12-10 18:27:01 +0700

    done

--- app/src/main/java/com/example/random/MainActivity.java
+++ app/src/main/java/com/example/random/MainActivity.java
@@ -4,15 +4,101 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    //1 khai báo biên
+    TextView mTvBieuthuc,mTvKetqua;
+    ImageButton mImgButtonDung,mImgButtonSai;
+    int mSothu1,mSothu2,mKetqua,mPheptinh;
+    boolean mRandomketqua = false;
+    Random mRandom;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //2 ánh sạ
+        mTvBieuthuc = findViewById(R.id.textViewBieuthuc);
+        mTvKetqua = findViewById(R.id.textViewKetqua);
+        mImgButtonDung = findViewById(R.id.imageButtonDung);
+        mImgButtonSai = findViewById(R.id.imageButtonSai);
+
+        // Task
+        //1 : Random biet thuc
+        //2 : Radom ket qua
+        //3 : Kiem tra ket qua
+
+        // random gia tri
+        mSothu1 = mSothu2= mKetqua = mPheptinh = 0;
+        mRandom = new Random();
+
+        mSothu1 = mRandom.nextInt(10) + 1;
+        mSothu2 = mRandom.nextInt(10) + 1;
+//        Log.d("BBB","So thu 1 :"+ mSothu1);
+//        Log.d("BBB","So thu 2 :"+ mSothu2);
+        mPheptinh = mRandom.nextInt(4)+1;
+        mRandomketqua = mRandom.nextBoolean();
+        // 1: cong , 2: tru , 3: nhan, 4: chia
+
+
+        switch (mPheptinh){
+            case 1 : mKetqua = mSothu1 + mSothu2;
+                mTvBieuthuc.setText(mSothu1 + " + " + mSothu2);
+                break;
+            case 2 : mKetqua = mSothu1 - mSothu2;
+                mTvBieuthuc.setText(mSothu1 + " - " + mSothu2);
+                break;
+            case 3 : mKetqua = mSothu1 * mSothu2;
+                mTvBieuthuc.setText(mSothu1 + " * " + mSothu2);
+                break;
+            case 4 : mKetqua = mSothu1 / mSothu2;
+                mTvBieuthuc.setText(mSothu1 + " / " + mSothu2);
+                break;
+        }
+        //        double result4=Math.round(Math.random()*5);
+        if (mRandomketqua == false){
+         mKetqua += mRandom.nextInt(5)+1;
+        }
+//        mKetqua += !mRandomketqua ? mRandom.nextInt(5)+1:0;
+        mTvKetqua.setText(" = " + mKetqua);
+        //su kien click cua imagebuton
+        mImgButtonDung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if  (mRandomketqua== true){
+
+                    Toast.makeText(MainActivity.this, "chinh xac", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Sai roi", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        mImgButtonSai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if  (mRandomketqua== false){
+
+                    Toast.makeText(MainActivity.this, "chinh xac", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Sai roi", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+//        call back funtion
+//      khi click vao imageviewButton
+
+//        kiem tra view có ton tai hay ko
+//        kieu tra vung nho cua view da dc khoi tao chua
+//        thuc hien su kien click
+
         //build in function : các phương thức sẽ đc gọi trực tiếp
        // mà ko cần khởi tạo lớp đối tượng
 
